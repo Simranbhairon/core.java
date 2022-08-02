be64daadf506b42d6a4da4a2200f16c5f1622f77
Simran <109948125+Simranbhairon@users.noreply.github.com>
2022-08-02 13:45:30 +0530

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,158 @@
+  // Q1 -- 
+ //import java.util.*;
+ //class Test2{
+  // public static void main( String [] args){
+   //  Scanner sc = new Scanner(System.in);
+	// int a=sc.nextInt();
+	 
+	// if (a%2==0)
+	// {
+	// System.out.println("number is even");
+	// }
+	// else 
+	// {
+	// System.out.println("no.is odd");
+	 //}
+	 //}
+	 //}
+	 
+	 // Q2 --
+	// import java .util.*;
+	// class Test2{
+	//	 public static void main(String [] args){
+	//		 Scanner sc = new Scanner (System.in);
+	//		 int a = sc .nextInt();
+	//		 if(a>55){
+	//			 System.out.println("number is greater than 55");
+	//
+//	}
+	//		 else if(a==55){
+		//		 System.out.println("number is equal to 55");
+			// }
+			 //else{
+				// System.out.println("number is smaller than 55");
+			// }
+		 //}
+	 //}
+	 
+	 // Q3 --
+	// import java.util.*;
+	// class Test2{
+	//	 public static void main(String [] args){
+	//		 Scanner sc = new Scanner (System.in);
+	//		 int a = sc.nextInt();
+	//		 int b =sc.nextInt();
+	//		 if( a==b){
+	//			 System.out.println("Equal");
+	//		 }
+	//			 else if(a>b){
+	//				 System.out.println(a);
+	//			 }
+	//			 else {
+	//				 System.out.println(b);
+	//			 }
+	//		 }
+	 //}
+	 
+	// Q4 --
+	//class Test2{
+		//public static void main(String [] args){
+	//	int rollno = 546;
+	//	switch (rollno) {
+	//		case 579 :
+	//		System.out.println("Sonu's rollno");
+	//		break;
+		//	case 546 :
+//			System.out.println("Monu's rollno");
+	//		break;
+		//	case 553 :
+			//System.out.println("Manu's rollno");
+	//		break;
+		//	case 544:
+	//		System.out.println("Meenu's rollno");
+		//	break;
+		//	case 516 :
+		//	System.out.println("Meena's rollno");
+		//	break;
+		//	default :
+		//	System.out.println("Not Allowed");
+		//	break;
+		//}
+		//}
+	//}
+	
+	// Q5
+	// in pass by value java passes the copy not the original file and here we call the function by value hence called pass by value 
+	//and in pass by refrence the memeory refrence is passed .in pass by reference the original file is passed. java only supports 
+	// pass by not pass by refrence.e.g.pass by value
+	   //class Test2{
+		 //  public static void main(String args[]){
+		//	   int number=24;
+		//	   System.out.println("before calling display"number);
+		//	   display( );
+		//	   System.out.println("After calling dispaly"number);
+		//   }
+		//	   public static void display(int num){
+		//		    System.out.println("inside the display ="+num);
+		//	   }
+	//	   }
+	//   }
+				   
+	
+	//Q6
+  //import java .util.*;	
+	//class Test2 {
+		//public static void main(String [] args){
+		//	Scanner sc =new Scanner (System.in);
+		//	System.out.println("enter A number"+n);
+		//	int n=sc.nextInt();
+		//	for(int i=1;i<=n;i++)
+		//	{
+		//		System.out.println(i);
+		//	}
+		//}
+	//}
+	
+		//Q8 
+		//  class Test2{
+  // public static void main(String args[])
+ // {
+//  int n=3;
+ // int m=4;
+  
+ // for(int i=1;i<=n;i++){
+ // int num;
+ //  if ((i%2)==0) {
+//		num=0;
+//	System.out.print(num);
+	//}
+	//else{
+	//	num=1;
+		//System.out.print(num);
+	//}
+	//for(int j=1;j<=m;j++){
+	//if(num==1)
+	//{
+		//num=0;
+		//System.out.print(num);
+  //}
+  //else{
+	//  num=1;
+//System.out.print(num);
+ // }
+ // }
+//	System.out.println( );
+	
+//	}
+//	}
+//	}
+
+   
+   
+   
+ 
+	
+	
+	
+	
+  
